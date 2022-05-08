6c3b1851b0d8bdd47a90481cc667079c487a5284
pvedernikov <pvedernikov@gmail.com>
2022-05-08 21:15:29 +0500

    Loader & pages

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -1,12 +1,25 @@
 import axios from 'axios';
 
 export default class PostService {
-    static async getAll() {
-        try {
-            const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-            return response.data;
-        } catch (e) {
-            console.log(e);
-        }
+    //static async getAll() {
+    //    const response = await axios.get('https://jsonplaceholder.typicode.com/111posts');
+    //    return response.data;
+
+    //    //try {
+    //    //    const response = await axios.get('https://111jsonplaceholder.typicode.com/posts');
+    //    //    return response.data;
+    //    //} catch (e) { // не нужно, т.к. ошибка обрабатывается на уровне нашего хука useFetching
+    //    //    console.log(e);
+    //    //}
+    //}
+    static async getAll(limit = 10, page = 1) {
+        //const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10&_page=2');
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+            params: {
+                _limit: limit,
+                _page: page
+            }
+        });
+        return response;
     }
 }
