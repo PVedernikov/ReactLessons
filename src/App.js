243801190ffd76eb926371527e8c5e77a31e5eb5
pvedernikov <pvedernikov@gmail.com>
2022-05-30 23:03:43 +0500

    context

--- src/App.js
+++ src/App.js
@@ -3,16 +3,33 @@ import "./styles/App.css";
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/UI/Navbar/Navbar';
 import AppRouter from './components/AppRouter';
+import { AuthContext } from './context';
 
 // PS D:\Projects\reactproject1> npm install react-transition-group --save
 // PS D:\Projects\reactproject1> npm i axios
 
 function App() {
+    const [isAuth, setIsAuth] = useState(false);
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (localStorage.getItem('auth')) {
+            setIsAuth(true);
+        }
+        setLoading(false);
+    }, []);
+
     return (
-        <BrowserRouter>
-            <Navbar />
-            <AppRouter />
-        </BrowserRouter>
+        <AuthContext.Provider value={{
+            isAuth,
+            setIsAuth,
+            isLoading
+        }}>
+            <BrowserRouter>
+                <Navbar />
+                <AppRouter />
+            </BrowserRouter>
+        </AuthContext.Provider>
     );
 }
 
--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,18 +1,60 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, { Component, useContext } from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import About from '../pages/About';
 import Posts from '../pages/Posts';
 import Error from '../pages/Error';
 import PostIdPage from '../pages/PostIdPage';
+import { publicRoutes, privateRoutes } from '../router';
+import { AuthContext } from '../context';
+import Loader from './UI/Loader/Loader';
 
 const AppRouter = () => {
+    const { isAuth, isLoading } = useContext(AuthContext);
+    console.log(isAuth);
+
+
+    if (isLoading) {
+        return <Loader />
+    }
+
     return (
-        <Routes>
-            <Route path="/about" element={<About />} />
-            <Route path="/posts" element={<Posts />} />
-            <Route path="/posts/:id" element={<PostIdPage />} />
-            <Route path="*" element={<Error />} />
-        </Routes>
+        isAuth
+            ? <Routes>
+                {/*<Route path="/about" element={<About />} />*/}
+                {/*<Route path="/posts" element={<Posts />} />*/}
+                {/*<Route path="/posts/:id" element={<PostIdPage />} />*/}
+                {/*<Route path="/" element={<Navigate replace to="/posts" />} />*/}
+                {/*<Route path="*" element={<Error />} />*/}
+                {privateRoutes.map(route =>
+                    <Route
+                        key={route.path}
+                        path={route.path}
+                        element={route.component}
+                        exact={route.exact}
+                    />
+                    //<Route
+                    //    path={route.path}
+                    //    element={<Component name={route.component} />}
+                    //    exact={route.exact}
+                    ///>
+                )}
+                <Route path="*" element={<Navigate replace to="/posts" />} />
+                {/*<Route path="/" element={<Navigate replace to="/posts" />} />*/}
+                {/*<Route path="*" element={<Error />} />*/}
+            </Routes>
+            : <Routes>
+                {publicRoutes.map(route =>
+                    <Route
+                        key={route.path}
+                        path={route.path}
+                        element={route.component}
+                        exact={route.exact}
+                    />
+                )}
+                <Route path="*" element={<Navigate replace to="/login" />} />
+                {/*<Route path="/" element={<Navigate replace to="/login" />} />*/}
+                {/*<Route path="*" element={<Error />} />*/}
+            </Routes>
     );
 };
 
